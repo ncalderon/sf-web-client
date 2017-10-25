51b9351daf9c30877214690d42527e603967f104
ncalderon <nathaniel.calderon@gmail.com>
2017-10-25 13:32:15 -0400

    Merge branch 'development' of https://github.com/ncalderon/sf-web-client into development

--- src/test/javascript/e2e/entities/bank.spec.ts
+++ src/test/javascript/e2e/entities/bank.spec.ts
@@ -74,19 +74,19 @@ export class BankDialogPage {
 
     setNameInput = function (name) {
         this.nameInput.sendKeys(name);
-    };
+    }
 
     getNameInput = function () {
         return this.nameInput.getAttribute('value');
-    };
+    }
 
     setDescriptionInput = function (description) {
         this.descriptionInput.sendKeys(description);
-    };
+    }
 
     getDescriptionInput = function () {
         return this.descriptionInput.getAttribute('value');
-    };
+    }
 
     save() {
         this.saveButton.click();
--- webpack/utils.js
+++ webpack/utils.js
@@ -16,12 +16,12 @@ function parseVersion() {
         if (err) {
             throw new Error('Failed to parse pom.xml: ' + err);
         }
-        if (result.project.version && result.project.version[0];) {
+        if (result.project.version && result.project.version[0]) {
             version = result.project.version[0];
         } else if (result.project.parent && result.project.parent[0] && result.project.parent[0].version && result.project.parent[0].version[0]) {
             version = result.project.parent[0].version[0];
         }
-})
+    });
     if (version === null) {
         throw new Error('pom.xml is malformed. No version is defined');
     }
