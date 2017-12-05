e9c93457602f9f5659d131dd61d624e7411612db
Nathaniel Calderon <nathaniel.calderon@gmail.com>
2017-12-05 16:02:06 -0400

    Fix issue on travis build

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_0bf0cf7d480d_key -iv $encrypted_0bf0cf7d480d_iv -in travis-env/secrets.tar.enc -out travis-env/secrets.tar -d
+openssl aes-256-cbc -K $encrypted_0bf0cf7d480d_key -iv $encrypted_0bf0cf7d480d_iv -in travis-env/secrets.tar.enc -out secrets.tar -d
 chmod +x scripts/deploy.sh
 chmod +x travis-env/secrets.tar
 tar xvf travis-env/secrets.tar
-chmod 600 travis-env/id_rsa
-chmod 600 travis-env/id_rsa.pub
-chmod 600 travis-env/config
+chmod 600 id_rsa
+chmod 600 id_rsa.pub
+chmod 600 config
+exit 0
