4523c293a0414905834dab403403d6d03c326245
ncalderon <nathaniel.calderon@gmail.com>
2019-08-07 00:12:06 -0400

    Testing travis

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+printenv
 openssl aes-256-cbc -K $encrypted_a08d1d0bac4b_key -iv $encrypted_a08d1d0bac4b_iv -in s-travis.tar.enc -out .\\s-travis.tar -d
 tar xvf s-travis.tar
 chmod +x mvnw
