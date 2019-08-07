c42fe18d8c0c17afc8552834ab52e4b543c77597
ncalderon <nathaniel.calderon@gmail.com>
2019-08-07 01:05:02 -0400

    Tests for travis

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -2,7 +2,7 @@
 openssl aes-256-cbc -K $encrypted_0bf0cf7d480d_key -iv $encrypted_0bf0cf7d480d_iv -in travis-env/secrets.tar.enc -out ./secrets.tar -d
 chmod +x secrets.tar
 tar xvf secrets.tar
-ll
+ls -la
 chmod 600 id_rsa
 chmod 600 id_rsa.pub
 chmod 600 config
